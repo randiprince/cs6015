17e5c8529cf8452ae65c57256f91944db8f8fa53
randiprince <u1431008@umail.utah.edu>
2023-02-02 13:38:50 -0700

    updates

--- MSDscript/test/run.cpp
+++ MSDscript/test/run.cpp
@@ -5,4 +5,13 @@
 //  Created by Randi Prince on 2/2/23.
 //
 
+extern "C" {
 #include "run.hpp"
+};
+
+#define CATCH_CONFIG_RUNNER
+#include "../MSDscript/catch.h"
+bool run_tests() {
+    const char *argv[] = { "test" };
+    return (Catch::Session().run(1, argv) == 0);
+}
--- MSDscript/test/run.hpp
+++ MSDscript/test/run.hpp
@@ -4,10 +4,4 @@
 //
 //  Created by Randi Prince on 2/2/23.
 //
-
-#ifndef run_hpp
-#define run_hpp
-
-#include <stdio.h>
-
-#endif /* run_hpp */
+extern bool run_tests(void);

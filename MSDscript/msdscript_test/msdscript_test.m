0318d7fba93b7f54f2944788a69488cabc49e8bf
randiprince <u1431008@umail.utah.edu>
2023-02-02 12:03:54 -0700

    updates

---
+++ MSDscript/msdscript_test/msdscript_test.m
@@ -0,0 +1,49 @@
+//
+//  msdscript_test.m
+//  msdscript_test
+//
+//  Created by Randi Prince on 2/2/23.
+//
+
+//#import <XCTest/XCTest.h>
+//
+//#import <XCTest/XCTest.h>
+//
+//@interface msdscript_test : XCTestCase
+//
+//@end
+//
+//@implementation msdscript_test
+//
+//- (void)setUp {
+//    // Put setup code here. This method is called before the invocation of each test method in the class.
+//}
+//
+//- (void)tearDown {
+//    // Put teardown code here. This method is called after the invocation of each test method in the class.
+//}
+//
+//- (void)testExample {
+//    // This is an example of a functional test case.
+//    // Use XCTAssert and related functions to verify your tests produce the correct results.
+//}
+//
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+//
+//@end
+
+#import <XCTest/XCTest.h>
+#include "run.hpp"
+@interface msdscript_test : XCTestCase
+@end
+@implementation msdscript_test
+- (void)testAll {
+    if (!run_tests())
+        XCTFail(@"failed");
+}
+@end
